12234cefa20ea338cb92e4516c1e3dc5a0b7bf5c
Joe Philip <joe.philip@hotmail.co.in>
2023-03-03 10:02:44 +0530

    added form component

--- src/components/component.js
+++ src/components/component.js
@@ -1,10 +1,12 @@
 import CardTitle from './card-title/component';
+import Form from './form/component';
 import './style.css';
 
 function Card() {
     return (
         <div id="card-container">
             <CardTitle title="Random password generator" />
+            <Form />
         </div>
     )
 }
