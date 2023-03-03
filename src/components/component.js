ed8a0f8a2adcaa26a812b533e66314fdd1775386
Joe Philip <joe.philip@hotmail.co.in>
2023-03-03 09:59:58 +0530

    added title component to card

--- src/components/component.js
+++ src/components/component.js
@@ -1,8 +1,10 @@
+import CardTitle from './card-title/component';
 import './style.css';
 
 function Card() {
     return (
         <div id="card-container">
+            <CardTitle title="Random password generator" />
         </div>
     )
 }
