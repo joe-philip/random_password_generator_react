44a7d0cc064ecd2b292c699133164b5c9e11163b
Joe Philip <joe.philip@hotmail.co.in>
2023-03-05 12:41:12 +0530

    defined footer component

---
+++ src/components/footer/component.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+function Footer() {
+    return (
+        <footer>Developed by
+            <a href='https://joe-philip.github.io/resume' target='_blank' className='footer-link'>Joe Philip</a>
+        </footer>
+    );
+}
+
+export default Footer;
