4b245f1abd75d45e9b4052afa7a33f2c42dac699
Joe Philip <joe.philip@hotmail.co.in>
2023-03-05 12:42:21 +0530

    added footer component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Card from './components/component';
+import Footer from './components/footer/component';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Card />
+    <Footer />
   </React.StrictMode>
 );
 
